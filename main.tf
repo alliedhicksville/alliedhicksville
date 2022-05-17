3903fb62cf46c6250f09dd8a57a69595c6618304
Nikhil Pawar <npawar@admproductions.com>
2022-05-17 01:05:39 -0400

    test terraform - code fix

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = vpc-0b6361e0f5fa95ceb
+  vpc_id      = "vpc-0b6361e0f5fa95ceb"
 
   /*
   ingress {
